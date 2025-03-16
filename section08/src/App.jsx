3151dbd9e75eb92a3b6ba4d85c1b4a15bc5c1e1d
ChoeSeulbi <hiitzsb@gmail.com>
2025-03-16 16:00:56 +0900

    [Editor.jsx - Create]

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,17 +1,47 @@
-import {useState} from "react";
+import {useState, useRef} from "react";
 import "./App.css";
-
 import Header from "./component/Header";
 import Editor from "./component/Editor";
 import List from "./component/List";
 
-function App() {
-  const [count, setCount] = useState(0);
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "study React1",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "study React2",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "study React3",
+    date: new Date().getTime(),
+  },
+];
 
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      dat: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
