bcfc37c5b06b8a83200625ae3eb93f907e45dcf6
ChoeSeulbi <hiitzsb@gmail.com>
2025-03-20 17:29:50 +0900

    [TodoList - useReduce]

--- section08/src/App copy.jsx
+++ section08/src/App copy.jsx
@@ -1,9 +1,9 @@
-import {useState, useRef, useReducer} from "react";
+import {useState, useRef} from "react";
 import "./App.css";
 import Header from "./component/Header";
 import Editor from "./component/Editor";
 import List from "./component/List";
-import Exam from "./component/Exam";
+
 const mockData = [
   {
     id: 0,
@@ -24,32 +24,19 @@ const mockData = [
     date: new Date().getTime(),
   },
 ];
-function reducer(state, action) {
-  switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
-      return state.map((item) =>
-        item.id === action.targetId ? {...item, isDone: !item.isDone} : item
-      );
-    case "DELETE":
-      return state.filter((item) => item.id !== action.targetId);
-  }
-}
+
 function App() {
-  const [todos, dispatch] = useReducer(reducer, mockData);
+  const [todos, setTodos] = useState(mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    dispatch({
-      type: "CREATE",
-      data: {
-        id: idRef.current++,
-        isDone: false,
-        content: content,
-        date: new Date().getTime(),
-      },
-    });
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
   };
 
   const onUpdate = (targetId) => {
@@ -57,16 +44,14 @@ function App() {
     // 타겟아이디와 일치하는 아이디를 갖는 투두아이템의 이즈던 변경
 
     //인수: 투두스 배열에서 타겟아이디와 일치하는 아이디를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
-    dispatch({
-      type: "UPDATE",
-      targetId: targetId,
-    });
+    setTodos(
+      todos.map((todo) =>
+        todo.id === targetId ? {...todo, isDone: !todo.isDone} : todo
+      )
+    );
   };
   const onDelete = (targetId) => {
-    dispatch({
-      type: "DELETE",
-      targetId: targetId,
-    });
+    setTodos(todos.filter((todo) => todo.id !== targetId));
   };
   return (
     <div className="App">
